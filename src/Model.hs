e66a394ed7d19db5f1ac9e8a7404b7bc22075567
Silas Peters <silaspeters03@gmail.com>
2022-10-13 16:55:51 +0200

    Alles gefixed tot aan classes

--- src/Model.hs
+++ src/Model.hs
@@ -1,18 +1,43 @@
 -- | This module contains the data types
 --   which represent the state of the game
 module Model where
-
-data InfoToShow = ShowNothing
-                | ShowANumber Int
-                | ShowAChar   Char
-
-nO_SECS_BETWEEN_CYCLES :: Float
-nO_SECS_BETWEEN_CYCLES = 5
-
-data GameState = GameState {
-                   infoToShow  :: InfoToShow
-                 , elapsedTime :: Float
-                 }
+import Distribution.Simple (registrationPackageDB)
 
 initialState :: GameState
 initialState = GameState ShowNothing 0
+
+-- Gamestate
+newtype Paused = Paused Bool
+newtype Time   = Time Int
+
+-- General stuff
+data GameState = GameState Player [Enemy] Time Paused
+newtype CoordX = CoordX Float
+newtype CoordY = CoordY Float
+newtype Health = Health Float
+newtype Size   = Size Float
+
+-- Entities
+newtype Player = Player CoordY
+data Astroid   = Astroid Size (CoordX, CoordY)
+data Alien     = Alien (CoordX, CoordY) Health
+
+-- Classes
+class Entity e where
+  move    :: e -> CoordX -> CoordY -> e
+  display :: e -> Picture
+
+class Entity p => Player p where
+  shoot :: p -> IO io
+
+class Entity e => Enemy e where
+  
+class Enemy e => AdvancedEnemy e where
+  shoot :: p -> IO io
+  
+
+-- instance Entity Player where
+--   move (Player y) _ dy = Player (y + dy) -- afhankelijk van keyboard
+
+-- instance Enemy Astroid where
+--   move (Player y) _ dy = Player (y + dy) -- afhankelijk van keyboard
--- src/View.hs
+++ src/View.hs
@@ -6,11 +6,13 @@ import Graphics.Gloss
 import Model
 
 view :: GameState -> IO Picture
-view gs = loadBMP "C:\\Users\\silas\\Dropbox\\Uni\\Functioneel Programmeren\\Game\\src\\wallpaper.bmp"
+view gs = do
+  wallpaper <- loadBMP "C:\\Users\\silas\\Dropbox\\Uni\\Functioneel Programmeren\\Game\\src\\wallpaper.bmp"
+  return . pictures $ wallpaper : [viewPure gs]
 
 viewPure :: GameState -> Picture
 -- viewPure gstate = case infoToShow gstate of
 --   ShowNothing   -> blank
 --   ShowANumber n -> color green (text (show n))
 --   ShowAChar   c -> color green (text [c])
-viewPure gameState = circle 80
+viewPure gameState = circleSolid 800
