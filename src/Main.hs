1ffe8b31f524320efeb68fa5558dd635e26af703
Silas Peters <silaspeters03@gmail.com>
2022-11-01 12:59:17 +0100

    stuff

--- src/Main.hs
+++ src/Main.hs
@@ -13,10 +13,10 @@ import System.Random
 screenSize = (1000, 600)
 initialState = GameState
   (Player (Coords (-350) 0) 500)   -- player
-  []                           -- keyList
+  []                               -- keyList
   [Alien   { pos = Coords 300 100,    rotation = 0, size = 1, speed = fst $ uniformR (10, 60) (mkStdGen 10), health = 1 },
    Astroid { pos = Coords 300 (-100), rotation = 0, size = 1, speed = fst $ uniformR (10, 30) (mkStdGen 70) }]
-          -- aliens
+        -- aliens
   0                            -- elapsed time
   False                        -- paused
   True                         -- alive
@@ -36,12 +36,12 @@ main = do
   alienImg     <- loadBMP imgAlien
   -- all IO is loaded, now run the game in a pure environment
   play (InWindow "Shoot'm up" screenSize (0, 0)) -- Or FullScreen
-              white              -- Background color
-              60                 -- Frames per second
-              initialState       -- Initial state
-              (view screenSize [("wallpaper", wallpaperImg),
-                                ("player", playerImg),
-                                ("astroid", astroidImg),
-                                ("alien", alienImg)])  -- View function
-              input              -- Event function
-              step               -- Step function
+        white              -- Background color
+        60                 -- Frames per second
+        initialState       -- Initial state
+        (view screenSize [("wallpaper", wallpaperImg),
+                          ("player",    playerImg),
+                          ("astroid",   astroidImg),
+                          ("alien",     alienImg)])  -- View function
+        input              -- Event function
+        step               -- Step function
