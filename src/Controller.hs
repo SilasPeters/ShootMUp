c6d60b7e8e8c92c97d690073a43a0aecf9d2171d
Silas Peters <silaspeters03@gmail.com>
2022-11-13 18:13:14 +0100

    2

--- src/Controller.hs
+++ src/Controller.hs
@@ -12,7 +12,7 @@ import Model
 import Data.Aeson as JSON
 --import Data.ByteString.Lazy.Internal
 --import Data.ByteString.Lazy.UTF8 as ByteStuff (ByteString)
-import Data.ByteString as ByteStuff (readFile)
+import Data.ByteString.Lazy as ByteStuff (readFile)
 
 -- | Handle one iteration of the game
 step :: Time -> GameState -> GameState
